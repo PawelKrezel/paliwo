b1146216acfb810eaa3a46a49a48dae59a3f9c85
Pawel Krezel <pawel.krezel@icloud.com>
2025-10-01 17:13:28 +0100

    In previous version the app would refresh upon submition and not display calculated values. I've been going through my old github repositories and discovered this forgotten issue. I decided to give it a quick fix. Also changed the last stored fuel consumption value to be displayed in a placeholder rather than be prefilled in the input field

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var index_koniec = ciasteczka.length;
 
 if (index_koniec > 9){
 	var zapisane_spalanie = ciasteczka.slice(9, index_koniec);
-	document.getElementById("spalanie").setAttribute("value", zapisane_spalanie.toString());
+	document.getElementById("spalanie").setAttribute("placeholder","Średnie spalanie (L/100KM). Ostatnio wprowadzone spalanie to "+zapisane_spalanie.toString()+" L/100KM");
 }
 
 
 function oblicz(){
-	var dystans = document.getElementById('dystans').value;
-	var spalanie = document.getElementById('spalanie').value;
+	var dystans = parseFloat(document.getElementById('dystans').value);
+	var spalanie = parseFloat(document.getElementById('spalanie').value);
 	document.cookie = "spalanie="+spalanie.toString();
-	var cena = document.getElementById('cena').value;
+	var cena = parseFloat(document.getElementById('cena').value);
 	
 	var litry = (dystans/100)*spalanie;
 	litry = litry.toPrecision(3);
